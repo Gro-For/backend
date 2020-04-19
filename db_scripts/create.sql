0db1566b4a1d1e875add69d42245cab8277ef37f
DuMoH112 <ew.dim@mail.ru>
2020-04-19 18:10:19 +0000

    Create index

--- db_scripts/create.sql
+++ db_scripts/create.sql
@@ -1,20 +1,28 @@
-create table users
+CREATE TABLE public.users
 (
     id SERIAL PRIMARY KEY,
-    firstname text,
-    lastname text,
-    patronymic text,
-    email text,
-    phone text,
-    country text,
-    city text,
-    inn int,
-    certificate bool,
-    intresting text,
-    url_instagram text,
-    url_vk text,
-    equipment_id int,
-    fertilizer text,
-    saleform text
+    firstname TEXT,
+    lastname TEXT,
+    patronymic TEXT,
+    email TEXT,
+    phone TEXT,
+    country TEXT,
+    city TEXT,
+    inn INT,
+    certificate BOOLEAN,
+    intresting TEXT,
+    url_instagram TEXT,
+    url_vk TEXT,
+    equipment_id INT,
+    fertilizer TEXT,
+    saleform TEXT,
+    UNIQUE(email)
 );
 
+CREATE INDEX users_email_index ON public.users USING btree(email);
+CREATE INDEX users_phone_index ON public.users USING btree(phone);
+CREATE INDEX users_country_index ON public.users USING btree(country);
+CREATE INDEX users_city_index ON public.users USING btree(city);
+
+
+INSERT INTO public.users(firstname,lastname,email,phone,country,city) VALUES('Петров', 'Володя', 'my_email@mail.ru','7(777)-777-7777','Canada','Vancouver');
