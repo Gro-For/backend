e2a1aec38ef355c95c050aa20327cb1381a23334
DuMoH112 <ew.dim@mail.ru>
2020-04-19 17:46:50 +0000

    Create class Database

--- backend/database/__init__.py
+++ backend/database/__init__.py
@@ -1,22 +1,44 @@
-# import psycopg2
+import psycopg2
 
 
-# class Database():
-#     cursor = None
+class Database():
+    __conn = None
 
-#     def __init__(self, config):
-#         self.conn = psycopg2.connect(
-#             host=config['POSTGRES']['HOST'],
-#             port=config['POSTGRES']['PORT'],
-#             database=config['POSTGRES']['DATABASE'],
-#             user=config['POSTGRES']['USER'],
-#             password=config['POSTGRES']['PASSWORD']
-#         )
-#         self.cursor = self.conn.cursor()
+    def __init__(self, config):
+        try:
+            print(
+                config['POSTGRES']['HOST'],
+                config['POSTGRES']['PORT'],
+                config['POSTGRES']['DATABASE'],
+                config['POSTGRES']['USER'],
+                config['POSTGRES']['PASSWORD']
+            )
+            self.__conn = psycopg2.connect(
+                host=config['POSTGRES']['HOST'],
+                port=config['POSTGRES']['PORT'],
+                dbname=config['POSTGRES']['DATABASE'],
+                user=config['POSTGRES']['USER'],
+                password=config['POSTGRES']['PASSWORD']
+            )
 
-#     def query(self, query):
-#         self.cursor.execute(query)
+            return True
+        except psycopg2.OperationalError:
+            return "База данных не доступна"
 
-#     def close(self):
-#         self.cursor.close()
-#         self.conn.close()
+    def __connect(self, config):
+        pass
+
+    def select_data(self, query):
+        cursor = self.__conn.cursor()
+        execute = cursor.execute(query)
+        cursor.close()
+        return execute.fetchall()
+
+    def insert_data(self, insert):
+        cursor = self.__conn.cursor()
+        cursor.execute(query)
+        cursor.close()
+        return True
+
+    def close(self):
+        self.__conn.close()
