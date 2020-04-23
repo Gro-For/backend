75489deb3a39b91d082e19799ca7288f2d50df3e
DuMoH112 <ew.dim@mail.ru>
2020-04-23 14:01:38 +0000

    Фиксирование вревя входа пользователя

--- backend/personal_area/auth.py
+++ backend/personal_area/auth.py
@@ -21,6 +21,7 @@ def auth_api():
     email = request.get_json(silent=True).get("email")
     password = request.get_json(silent=True).get("password")
     user_data = database.login({"email": email})
+
     if user_data:
         if check_password_hash(user_data[1], password):
             user = User()
@@ -28,5 +29,11 @@ def auth_api():
             user.set_role(user_data[2])
             user.set_email(email)
             user.auth(user)
+            user.set_last_login(database.select_data(
+                "SELECT last_login FROM public.users WHERE id={}".format(user.get_id()))[0][0])
+            database.insert_data("UPDATE users SET last_login=now() \
+                                WHERE id='{id}'".format(
+                id=user.get_id()
+            ))
             return jsonify({"UserToken": user.get_token(), "role": user.get_role()})
     return jsonify({'message': 'Invalid email or password'}), 401, {'ContentType': 'application/json'}
